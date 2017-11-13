3fe610bc9af6096472dfd87e873f0ce7fb4f233c
xSephiria <esrielthefallen@gmail.com>
2017-11-13 18:43:02 +0800

    player able to move, background added

---
+++ Assets/Scripts/playerBulletMovement.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerBulletMovement : MonoBehaviour {
+
+    public float speed;
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = transform.up * speed;
+    }
+}
---
+++ Assets/Scripts/playerControls.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerControls : MonoBehaviour {
+
+    float speed = 20;
+
+    [Header("Movement Boundaries")]
+    public float minX;
+    public float minY;
+    public float maxX;
+    public float maxY;
+
+	void FixedUpdate()
+    {
+        var rb = GetComponent<Rigidbody2D>();
+        Vector3 movement = new Vector3(0,0,0);
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            speed = 10;
+        else
+            speed = 20;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            movement.x = 10;
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            movement.x = -10;
+        else if (Input.GetKey(KeyCode.UpArrow))
+            movement.y = 10;
+        else if (Input.GetKey(KeyCode.DownArrow))
+            movement.y = -10;
+        else
+            movement.Set(0, 0, 0);
+
+        rb.velocity = movement * speed * Time.deltaTime;
+
+        // Prevents the player from moving out of the screen
+        rb.position = new Vector3
+            (
+            Mathf.Clamp(rb.position.x, minX, maxX),
+            Mathf.Clamp(rb.position.y, minY, maxY),
+            0.0f
+            );
+    }
+}
